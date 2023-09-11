e3ce6b96a620c8a11906b4d5bdffd7c92c4eadcb
李孔林 <weirongwei2016@126.com>
2023-09-11 22:57:06 +0800

    feat：首次提交java后台代码

---
+++ src/main/java/com/example/javatestone/JavaTestOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javatestone/JavaTestOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatestone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
